94b9fcfe8cce683c8625a7a1380995b58c2693a1
Emile <mimil2014@gmail.com>
2021-03-14 14:26:36 -0400

    afficher la zone en matlab lets go

--- moindre_Carre.m
+++ moindre_Carre.m
@@ -1,12 +1,28 @@
 %calcul moindre carre
 % x = (At * A )-1 * At * y
+hg = 0.1;
+hd = 0.05;
+lb = 0.15;
 
 Y = [0.013914,0.021067,0.039266,0.04597,0.053326]';
-A = [0.028686,0.008891,-0.040587,-0.060395,-0.080185]';
+Z = [0.028686,0.008891,-0.040587,-0.060395,-0.080185,
+     1,       1,        1,        1,        1]';
 
-x = inv(A'*A) * A' * Y;
+plot(Z,Y)
 
-X2 = A\Y
+%X1 = A' * inv(A*A') * Y
+X2 = inv(Z' * Z) * Z' * Y
+%X3 = A\Y
  % x et X2 donne la meme chose.
- 
-atand(x/1)
+%atand(X1)
+atand(X2)
+% trouver angle nominal 
+theta_nom = atand((hg-hd)/lb)
+
+diffenceAngle = abs(atand(X2(1))) - theta_nom
+
+% valider hauteur hg avec l'angle trouvé
+hg2 = tand(abs(atand(X2(1)))) * 0.15 + 0.05
+differenceHg = hg2 - hg
+
+%atand(X3)
